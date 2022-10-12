1f9aee14d1f0d75f046a5eb22eaec4a5222ba685
Samuel Bispo <79665216+samuellincon@users.noreply.github.com>
2022-10-12 15:03:20 -0300

    Adição do arquivo code.js

---
+++ codejs/code.js
@@ -0,0 +1,20 @@
+let allMusic = [
+  {
+    name: "Killing Verse",
+    artist: "Big Naughty",
+    img: "big",
+    src: "big"
+  },  
+  {
+    name: "NPR Concert",
+    artist: "Moon Hooch",
+    img: "moon",
+    src: "moon"
+  },
+  {
+    name: "NPR Concert",
+    artist: "Trueno",
+    img: "trueno",
+    src: "trueno"
+  },
+];
